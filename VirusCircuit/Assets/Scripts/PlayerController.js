6695e91e3d4c8c54e51d08cf895a4851ab928fb6
Damien Bargiacchi <icymidnight@gmail.com>
2013-01-26 14:16:11 -0800

    Basic Player controller

--- VirusCircuit/Assets/Scripts/PlayerController.js
+++ VirusCircuit/Assets/Scripts/PlayerController.js
@@ -4,6 +4,14 @@ function Start () {
 
 }
 
-function Update () {
+var speed : float = 100;
 
+function Update () {
+    var vMove : float = Input.GetAxis("Vertical")   * speed;
+    var hMove : float = Input.GetAxis("Horizontal") * speed;
+    
+    vMove *= Time.deltaTime;
+    hMove *= Time.deltaTime;
+    
+    transform.rigidbody.AddForce(hMove, 0, vMove);
 }
