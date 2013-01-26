d6f2c14f1b332651875e173449b83821e0a094c5
Damien Bargiacchi <icymidnight@gmail.com>
2013-01-26 13:57:37 -0800

    Parameterized flow function

--- VirusCircuit/Assets/Scripts/Flow.js
+++ VirusCircuit/Assets/Scripts/Flow.js
@@ -1,9 +1,46 @@
 #pragma strict
+// Manages the blood flow.
+// Internally the the cycle time is [0, 1).
+// Parameters to tune increasePeriod, decreasePeriod, flowSpeed
 
-function Start () {
-
-}
-
-function Update () {
-
+class Flow {
+	// How long to spend increasing the flow [0, increasePeriod)
+	private static var increasePeriod : float = 0.25;
+	// The multiplier we need to make sure to get half of the cosign period over the course of
+	// the inscreasing region.
+	private static var increaseFunctionSelector : float = (1.0 / increasePeriod) * Mathf.PI;
+	
+	// How long to spend decreasing the flow [decreaseStart, decreaseStart + decreasePeriod)
+	private static var decreasePeriod : float = 0.1;
+	private static var decreaseStart : float = 1.0 - decreasePeriod;
+	// The multiplier we need to make sure to get half of the cosign period over the course of
+	// the decreasing region.
+	private static var decreaseFunctionSelector : float = (1.0 / decreasePeriod) * Mathf.PI;
+	
+	private static var flowSpeed : float = 5;
+	
+	// Returns a cyclic value based on t of the blood flow.
+	// Starts at -0.25 and moves up to 1, then returns to -0.25
+	public static function flowAt(t : float) {
+		// Change the speed of the flow
+	    t = t / flowSpeed;
+	    
+	    // Make t be [0, 1) so that the flow cycles over some unit of time,
+	    // i.e. affected by the speed above.
+	    t = t - Mathf.Floor(t);
+	    
+	    if (t < increasePeriod) {  // The region of the cycle where we increase.
+	    	// Map (0, increasePeriod] into (0, PI/2]
+	        t *= increaseFunctionSelector;
+	        // Scale the cos to fit between -0.25 and 1, and move to the rising half of the cycle.
+	        return (0.75 * Mathf.Cos(t + Mathf.PI)) + 0.25;
+	    } else if (t < decreaseStart) {  // The region of the function where we flow forward.
+	        return 1;
+	    } else  {  // The region of the function where we decrease and eventually drop below 0.
+	        t = (t - decreaseStart) * decreaseFunctionSelector;
+	        // Map (decreaseStart, 1.0] into (0, PI/2]
+	        // Scale the cos to fit between -0.25 and 1.
+	        return (0.75 * Mathf.Cos(t)) + 0.25;
+	    }
+	}
 }
