973311c1be44410f54eac79dd7e6ce5a5b7a3ce6
Damien Bargiacchi <icymidnight@gmail.com>
2013-01-27 11:27:18 -0800

    New camera logic

--- VirusCircuit/Assets/Scripts/CameraUtils.js
+++ VirusCircuit/Assets/Scripts/CameraUtils.js
@@ -13,3 +13,41 @@ public function GetCurrentLevelSegment(origin : Camera) {
 	}
 	return null;
 }
+
+public function GetCurrentLevelSegment2(origin : Vector3) {
+	var hitInfo : RaycastHit;
+	if (Physics.Raycast(origin, Vector3(0, -1, 0), hitInfo, Mathf.Infinity, layerMask)) {
+		//var body : Rigidbody = hitInfo.rigidbody;
+		var collider : Collider = hitInfo.collider;
+		if (collider != null) {
+			Debug.Log(collider.transform.parent.tag + " - " + collider.bounds.size);
+			return collider;
+		}
+	}
+	return null;
+}
+
+public function GetCurrentLevelSegment(origin : GameObject) {
+	var hitInfo : RaycastHit;
+	if (Physics.Raycast(origin.transform.position, Vector3(0, -1, 0), hitInfo, Mathf.Infinity, layerMask)) {
+		//var body : Rigidbody = hitInfo.rigidbody;
+		var collider : Collider = hitInfo.collider;
+		if (collider != null) {
+			return collider;
+		}
+	}
+	return null;
+}
+
+public function FindNextLevelSegment(currentSegment : Transform, forward : Vector3) {
+	var length : int = 0;
+	if (currentSegment.CompareTag("Straight")) {
+		length = 8;
+	} else if (currentSegment.CompareTag("Curve")) {
+		length = 7;
+	}
+	var currentCentre : Vector3 = currentSegment.transform.position;
+	var normalizedForward : Vector3 = forward.normalized;
+	var newStart = currentCentre + (normalizedForward * length);
+	return GetCurrentLevelSegment(newStart);
+}
