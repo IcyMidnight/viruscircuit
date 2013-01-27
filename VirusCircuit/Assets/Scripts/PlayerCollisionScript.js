f905a79742cf2565a02ec0883d376298fb4ec552
Daniel Clayson <danclayson@gmail.com>
2013-01-26 22:30:54 -0800

    New Update to Virus

--- VirusCircuit/Assets/Scripts/PlayerCollisionScript.js
+++ VirusCircuit/Assets/Scripts/PlayerCollisionScript.js
@@ -6,26 +6,31 @@ var collisionBVW : AudioClip;
 var TargetCell : AudioClip;
 
 
+
 function OnCollisionEnter (collision : Collision) {
 	
-//		Debug.Log ("Collision");
 		
-		//Interact with White Blood Cell Collision
-		if(collision.gameObject.tag == "WBC"){
-		Debug.Log("WBC");
-		audio.PlayOneShot(collisionWBC);
+		for (var contact : ContactPoint in collision.contacts){
 		
-		}else if(collision.gameObject.tag == "NPC"){
-		Debug.Log("NPC");
-		audio.PlayOneShot(collisionNPC);
 		
-		} else if(collision.gameObject.tag == "BVW"){
 		
-		audio.PlayOneShot(collisionBVW);
-		
-		}else if(collision.gameObject.tag == "TargetCell"){
-		
-		audio.PlayOneShot(TargetCell);
-		
+			//Interact with White Blood Cell Collision
+			if(collision.gameObject.tag == "WBC"){
+			Debug.Log("WBC");
+			audio.PlayOneShot(collisionWBC);
+			
+			}else if(collision.gameObject.tag == "NPC"){
+			Debug.Log("NPC");
+			audio.PlayOneShot(collisionNPC);
+			
+			} else if(collision.gameObject.tag == "BVW"){
+			
+			audio.PlayOneShot(collisionBVW);
+			
+			}else if(collision.gameObject.tag == "TargetCell"){
+			
+			audio.PlayOneShot(TargetCell);
+			
+			}
 		}
 }
